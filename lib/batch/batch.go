534affdc4b647a879c59cd1886db5182fc408140
Kolodiazhnyi Serhii <elkrot@gmail.com>
2022-07-01 12:31:30 +0300

    Update batch.go
    first commit

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -13,6 +14,18 @@ func getOne(id int64) user {
 	return user{ID: id}
 }
 
-func getBatch(n int64, pool int64) (res []user) {
-	return nil
+func getBatch(n int64, pool int64) (res []user) {	
+	var wg sync.WaitGroup
+	sem := make(chan user, pool)
+	for i := 0; i < int(n); i++ {
+		wg.Add(1)
+		go func(j int) {
+			user := getOne(int64(j))
+			sem <- user
+			res = append(res, <-sem)
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	return res
 }
